793bcee6456166bd0e8fc3bf6554ed4c61e878fd
виктор еремеев <a62wap@mail.ru>
2023-07-25 16:43:45 +0300

    25_07_23

--- stepik_vacancies/vacancies/views.py
+++ stepik_vacancies/vacancies/views.py
@@ -5,12 +5,26 @@ from vacancies.models import Company, Speciality, Vacancy
 class MainPageView(View):
     def get(self, request):
         specialities = Speciality.objects.all()
-        return render(request, 'index.html', {'specialities': specialities})
+        spec_data = []
+        for spec in specialities:
+            spec_data.append({
+                'cnt': len(Vacancy.objects.filter(speciality=spec.id)), # type: ignore
+                'spec': spec
+                })  
+        companies = Company.objects.all()
+        comp_data = []
+        for comp in companies:
+            comp_data.append({
+                'cnt': len(Vacancy.objects.filter(company=comp.id)),
+                'comp': comp
+            })
+        return render(request, 'index.html', {'specialities_data': spec_data, 'companies_data': comp_data})
 
 
 class AllVacanciesView(View):
     def get(self, request):
-        return render(request, 'vacancies.html')
+        vacancies = Vacancy.objects.all()
+        return render(request, 'vacancies.html', {'vacancies_data': vacancies, 'title': 'Все вакансии'})
 
 
 class VacanciesByCatView(View):
